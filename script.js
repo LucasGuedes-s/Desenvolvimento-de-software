d76af6fc1e9476b3abf877f5e05725809a1ac23c
Lucas <lucassguedes2908@gmail.com>
2022-11-14 17:33:16 -0300

    Adicionando as funções de Multiplicação e Divisão

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ let operacao;
 let escolha;
 
 do{
-    num1 = prompt("Digite um número: ")
-    num2 = prompt("Digite outro número: ")
-    
+
     operacao = prompt`
-    somar ............. 1
-    subtrair .......... 2
+    
+    Somar ............. 1
+    Subtrair .......... 2
+    Multiplicar........ 3
+    Dividir............ 4
 
     Qual operação deseja execultar ? 
     `
+    num1 = prompt("Digite um número: ")
+    num2 = prompt("Digite outro número: ")
+    
+    
     if(operacao == "1"){
         function soma(num1,num2){
             return parseFloat(num1) + parseFloat(num2)
@@ -25,6 +30,22 @@ do{
         }
         alert(subtracao(num1,num2))
     }
+    else if(operacao == "3"){
+        function multiplicar(num1,num2){
+            return Number(num1)*Number(num2)
+        }
+        alert(multiplicar(num1,num2))
+    }
+    else if(operacao == "4"){
+        function Dividir(num1,num2){
+            return Number(num1)/Number(num2)
+        }
+        alert(Dividir(num1,num2))
+    }
+    else{
+        alert("Operação Inválida!")
+    }
 
     escolha = prompt("Deseja continuar? S/N")
-}while(escolha != "s" || escolha != "S")
+
+}while(escolha == "s" || escolha == "S")
